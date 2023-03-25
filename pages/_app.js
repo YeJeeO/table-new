7c360e90c23ea95527c3658b91948bff1d725e62
YeJeeO <yejeeo@yandex.ru>
2023-03-25 10:10:38 +0700

    renamed:    components/App.js -> components/NewTable.js modified:   pages/_app.js modified:   pages/index.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,8 @@
 import '../pages/style.css';
+import NewTable from "../components/NewTable";
 
 export default function MyApp({ Component, pageProps }) {
-  return <><Component {...pageProps} /></>;
+  return <><Component {...pageProps} />
+  <NewTable/> 
+  </>;
 }
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
-import App from "../components/App";
+
 
 
 export default function HomePage(){
 
     return <><h1>New table</h1>
-    <App/> </>
+    </>
   }
